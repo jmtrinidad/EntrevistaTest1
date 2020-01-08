d71288a2e211618abb4be5957df07b7e842238fb
Jose Miguel Trinidad R <JM.trinidad.99@hotmail.com>
2020-01-08 18:24:34 -0400

    Mover to branch

--- EntrevistaTest1.ConsoleReadTxtFile/Program.cs
+++ EntrevistaTest1.ConsoleReadTxtFile/Program.cs
@@ -15,8 +15,11 @@ namespace EntrevistaTest1.ConsoleReadTxtFile
             int counter = 0;
             string line;
             var addresses = new List<Address>();
-            // Read the file and display it line by line.  
-            StreamReader file = new StreamReader(@"g:\test.txt");
+            // Read the file and display it line by line.
+            //NOTA: CUANDO PONES LA DIRECCION DE ESA MANERA ES PORQUE EL ARCHIVO
+            //ESTA DENTRO DE LA MISMA CARPETA DE LA SOLUCION DE TU APLICACION
+            StreamReader file = new StreamReader(@"\test.txt");
+
             while ((line = file.ReadLine()) != null)
             {
                 var address = new Address
